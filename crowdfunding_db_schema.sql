6b91a09cf5f70dfe374beda7704e09ee5c2edb4e
J. Viloria-Paolini <50921917+biloriaj@users.noreply.github.com>
2023-05-25 20:03:20 -0500

    Add files via upload

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,53 @@
+
+CREATE TABLE Category (
+  category_id VARCHAR(300) NOT NULL PRIMARY KEY,
+  category VARCHAR(300) NOT NULL
+);
+
+
+CREATE TABLE Subcategory (
+  subcategory_id VARCHAR(300) NOT NULL PRIMARY KEY,
+  subcategory VARCHAR(300) NOT NULL
+);
+
+
+CREATE TABLE Contacts (
+  contact_id INTEGER NOT NULL PRIMARY KEY,
+  first_name VARCHAR(300) NOT NULL,
+  last_name VARCHAR(300) NOT NULL,
+  email VARCHAR(300) NOT NULL
+);
+
+
+CREATE TABLE Campaign (
+  cf_id INTEGER NOT NULL PRIMARY KEY,
+  contact_id INTEGER NOT NULL,
+  company_name VARCHAR(300) NOT NULL,
+  description VARCHAR(300) NOT NULL,
+  goal FLOAT NOT NULL,
+  pledged FLOAT NOT NULL,
+  outcome VARCHAR(300) NOT NULL,
+  backers_count INTEGER NOT NULL,
+  country VARCHAR(300) NOT NULL,
+  currency VARCHAR(300) NOT NULL,
+  launched_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  category_id VARCHAR(300) NOT NULL,
+  subcategory_id VARCHAR(300) NOT NULL,
+  FOREIGN KEY (contact_id) REFERENCES Contacts(contact_id),
+  FOREIGN KEY (category_id) REFERENCES Category(category_id),
+  FOREIGN KEY (subcategory_id) REFERENCES Subcategory(subcategory_id)
+);
+
+
+SELECT * FROM Campaign;
+SELECT * FROM Category;	
+SELECT * FROM Subcategory;
+SELECT * FROM Contacts;
+
+
+
+
+
+
+
